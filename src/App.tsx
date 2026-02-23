516bab2200736066918196a1447098cc06ce1467
santosbeitrande-art <santosbeitrande@gmail.com>
2026-02-23 22:01:19 +0200

    Replace App component with QRScanner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
+import QRScanner from './components/QRScanner'
+
 export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1 style={{ fontSize: 40, color: "black" }}>
-        APP FUNCIONA
-      </h1>
-    </div>
-  )
+  return <QRScanner />
 }
