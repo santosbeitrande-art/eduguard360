a2f1f9ed4e21af906e3543ba37e026a32c0071f0
santosbeitrande-art <santosbeitrande@gmail.com>
2026-03-13 21:18:54 +0200

    Add StudentCard component with name and classroom props

--- src/components/StudentCard.tsx
+++ src/components/StudentCard.tsx
@@ -1,1 +1,24 @@
+interface Props{
+name:string
+classroom:string
+}
 
+export default function StudentCard({name,classroom}:Props){
+
+return(
+
+<div className="bg-white shadow p-4 rounded">
+
+<h2 className="font-bold">
+{name}
+</h2>
+
+<p className="text-gray-600">
+Turma: {classroom}
+</p>
+
+</div>
+
+)
+
+}
