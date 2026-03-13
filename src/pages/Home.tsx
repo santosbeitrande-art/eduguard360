d0a377ec3db1091280cb50bf4b878dec6689ae1f
santosbeitrande-art <santosbeitrande@gmail.com>
2026-03-13 21:35:59 +0200

    Implement Home page with Navbar and content
    Added a Navbar component and content for the Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,27 @@
+import Navbar from "@/components/Navbar"
 
+export default function Home(){
+
+return(
+
+<div>
+
+<Navbar/>
+
+<div className="text-center mt-20">
+
+<h1 className="text-4xl font-bold">
+Segurança Escolar Digital
+</h1>
+
+<p className="mt-4">
+Controle de entradas e saídas de alunos
+</p>
+
+</div>
+
+</div>
+
+)
+
+}
