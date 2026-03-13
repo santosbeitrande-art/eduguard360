76a6b3131b012eb66d8086804148ec340d77d47f
santosbeitrande-art <santosbeitrande@gmail.com>
2026-03-13 21:43:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-import QRScanner from './components/QRScanner'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
-export default function App() {
-  return <QRScanner />
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+import Students from "./pages/Students"
+import Checkin from "./pages/Checkin"
+
+function App(){
+
+return(
+
+<BrowserRouter>
+
+<Routes>
+
+<Route path="/" element={<Home/>}/>
+<Route path="/login" element={<Login/>}/>
+<Route path="/dashboard" element={<Dashboard/>}/>
+<Route path="/students" element={<Students/>}/>
+<Route path="/checkin" element={<Checkin/>}/>
+
+</Routes>
+
+</BrowserRouter>
+
+)
+
 }
+
+export default App
