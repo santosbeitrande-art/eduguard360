17ef4f916cf98e6447bcc16a0581b000e24380e3
santosbeitrande-art <santosbeitrande@gmail.com>
2026-03-13 23:00:00 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
+import Index from "./Index"
 
 ReactDOM.createRoot(
 document.getElementById("root")!
 ).render(
 
 <React.StrictMode>
-<App/>
+<Index/>
 </React.StrictMode>
 
 )
