14a3c7f14b571def20a2a29352f276afbc4d234f
santosbeitrande-art <santosbeitrande@gmail.com>
2026-03-13 21:18:23 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,30 @@
+import { Link } from "react-router-dom"
 
+export default function Navbar() {
+
+return(
+
+<nav className="bg-blue-700 text-white p-4">
+
+<div className="max-w-6xl mx-auto flex justify-between">
+
+<h1 className="font-bold text-xl">
+EduGuard360
+</h1>
+
+<div className="flex gap-4">
+
+<Link to="/">Home</Link>
+<Link to="/dashboard">Dashboard</Link>
+<Link to="/students">Alunos</Link>
+<Link to="/checkin">Entrada</Link>
+
+</div>
+
+</div>
+
+</nav>
+
+)
+
+}
