5773e670367ebed3aeb54c00819ac22c23270a8c
santosbeitrande-art <santosbeitrande@gmail.com>
2026-03-13 21:39:07 +0200

    Create Dashboard component with layout and Navbar
    Added a Dashboard component with a Navbar and layout.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,1 +1,39 @@
+import Navbar from "@/components/Navbar"
 
+export default function Dashboard(){
+
+return(
+
+<div>
+
+<Navbar/>
+
+<div className="p-10">
+
+<h1 className="text-3xl font-bold mb-6">
+Dashboard
+</h1>
+
+<div className="grid grid-cols-3 gap-4">
+
+<div className="bg-white p-6 shadow rounded">
+Alunos
+</div>
+
+<div className="bg-white p-6 shadow rounded">
+Entradas Hoje
+</div>
+
+<div className="bg-white p-6 shadow rounded">
+Saídas Hoje
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+)
+
+}
